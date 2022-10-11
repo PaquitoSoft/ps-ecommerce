e77264b039cd8fe4788055d5a3cf363311afc266
PaquitoSoft <paquitosoftware@gmail.com>
2022-10-11 16:52:27 +0200

    refactor(app-customer): debug middleware

--- apps/customer/pages/_middleware.mid.ts
+++ apps/customer/pages/_middleware.mid.ts
@@ -1,27 +1,42 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { CookieSerializeOptions } from 'next/dist/server/web/types';
+// import { CookieSerializeOptions } from 'next/dist/server/web/types';
 
 import { constants, utils } from '@ps-ecommerce/shared-ui-logic';
 
 
 export function middleware(request: NextRequest) {
-	let userId = request.cookies[constants.USER_COOKIE_NAME];
-	const cookieAttributes: CookieSerializeOptions = {
-		httpOnly: true
-	};
+	console.log('Middleware# Original request:', request.nextUrl);
+	const response = NextResponse.next();
 
-	if (process.env.NODE_ENV === 'production') {
-		cookieAttributes.secure = true;
-		cookieAttributes.sameSite = 'none';
-	}
+	let userId = request.cookies[constants.USER_COOKIE_NAME];
 
 	if (!userId) {
+		// const cookieAttributes: CookieSerializeOptions = {
+		// 	httpOnly: true
+		// };
+		// if (process.env.NODE_ENV === 'production') {
+		// 	cookieAttributes.secure = true;
+		// 	cookieAttributes.sameSite = 'none';
+		// }
 		userId = utils.generateId();
-		return NextResponse
-			.rewrite(request.nextUrl)
-			.cookie(constants.USER_COOKIE_NAME, userId, cookieAttributes);
+		response.cookie(constants.USER_COOKIE_NAME, userId, {
+			httpOnly: true,
+			...((process.env.NODE_ENV === 'production') ? {
+				secure: true,
+				sameSite: 'none'
+			} : {})
+		});
+		// return NextResponse
+		// 	.rewrite(request.nextUrl)
+		// 	.cookie(constants.USER_COOKIE_NAME, userId, cookieAttributes);
 	}
 
+	// console.log('Middleware# Setting userId value in an incoming request header:', userId);
+	// request.headers.append(constants.USER_ID_HEADER, userId);
+	// console.log('Middleware# Incoming request headers:', request.headers);
+
 	request.nextUrl.searchParams.set('userId', userId);
+	console.log('Middleware# Rewriting request to:', request.nextUrl);
 	return NextResponse.rewrite(request.nextUrl);
+	// return response;
 }
