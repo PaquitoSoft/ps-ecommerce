bbf1e173868e5607b17f58d67e610074f4bfa8c8
PaquitoSoft <paquitosoftware@gmail.com>
2022-11-22 20:21:01 +0100

    feat(customer-service): implement create order mutation

---
+++ apps/customer-service/src/application/use-cases/order/create-order-action.ts
@@ -0,0 +1,35 @@
+import { v4 as uuiv4 } from 'uuid';
+import type { Order } from '@ps-ecommerce/types';
+import type NewOrder from '../../../types/new-order';
+import OrderRepository from '../../../domain/order-repo';
+
+function getRandomDeliveryDate() {
+	const d = new Date();
+	const randomDays = Math.random() * (5 - 1) + 1; // Ger a random day between 1 and 5
+	d.setDate(d.getDate() + Math.trunc(randomDays));
+	return d;
+}
+
+async function createOrderAction(
+	newOrder: NewOrder,
+	{ orderRepository }: { orderRepository: OrderRepository }
+): Promise<Order> {
+	const totals = newOrder.items.reduce<{ units: number, amount: number }>((agg, item) => {
+		return {
+			units: agg.units + item.quantity,
+			amount: agg.amount + item.quantity * item.product.price,
+		}
+	}, { units: 0, amount: 0 });
+
+	return orderRepository.create({
+		...newOrder,
+		code: uuiv4().split('-')[0].toUpperCase(),
+		totalUnits: totals.units,
+		totalAmount: totals.amount,
+		placedDate: Date.now(),
+		estimatedDeliveryDate: getRandomDeliveryDate().getTime(),
+		deliveryCost: 0,
+	});
+}
+
+export default createOrderAction;
--- apps/customer-service/src/infrastructure/web/apollo/types/order-gql-type.ts
+++ apps/customer-service/src/infrastructure/web/apollo/types/order-gql-type.ts
@@ -1,10 +1,12 @@
 import { gql } from "@apollo/client";
+import { Order } from "@ps-ecommerce/types";
 
 import ApolloContext from '../apollo-context';
 
 import getUserLastOrdersAction from "../../../../application/use-cases/order/get-user-last-order-action";
 import getUserOrderAction from "../../../../application/use-cases/order/get-user-order-action";
 import getUserOrdersAction from "../../../../application/use-cases/order/get-user-orders-action";
+import createOrderAction from "../../../../application/use-cases/order/create-order-action";
 
 export const typeDef = gql`
 	type OrderItemProduct {
@@ -37,10 +39,55 @@ export const typeDef = gql`
 		paymentData: OrderPaymentData!
 	}
 
+	input NewOrderItemProduct @inaccessible {
+		code: String!
+		name: String!
+		sizeCode: String!
+		sizeName: String!
+		price: Float!
+		colorName: String!
+		imageUrl: String!
+	}
+
+	input NewOrderItem @inaccessible {
+		product: NewOrderItemProduct!
+		quantity: Int!
+	}
+
+	input NewOrderShippingAddress @inaccessible {
+		email: String!
+		name: String!
+		surname: String!
+		addressLine: String!
+		postalCode: String!
+		city: String!
+	}
+
+	input NewPaymentCreditCard @inaccessible {
+		pan: String!
+		cardholder: String!
+	}
+
+	input NewOrderPaymentData @inaccessible {
+		paymentMethod: String!
+		paymentDetails: NewPaymentCreditCard!
+	}
+
+	input NewOrder @inaccessible {
+		userId: String!
+		items: [NewOrderItem]!
+		shippingAddress: NewOrderShippingAddress!
+		paymentData: NewOrderPaymentData!
+	}
+
 	extend type Query {
 		userOrders: [Order]!
 		userLastOrder: Order
 		order(orderCode: String!): Order
+	}
+
+	extend type Mutation {
+		createOrder(newOrder: NewOrder!): Order @inaccessible
 	}
 `;
 
@@ -87,6 +134,16 @@ const getUserLastOrder = async (
 	return orders;
 };
 
+const createOrder = (
+	_root: unknown,
+	args: { newOrder: Order },
+	context: ApolloContext
+) => {
+	return createOrderAction(args.newOrder, {
+		orderRepository: context.dataSources.order
+	});
+};
+
 export const resolvers = {
 	Order: {
 		id: (root: { _id: unknown; id: unknown; }) => root._id || root.id
@@ -95,5 +152,8 @@ export const resolvers = {
 		userOrders: getUserOrdersList,
 		userLastOrder: getUserLastOrder,
 		order: getUserOrderDetails
+	},
+	Mutation: {
+		createOrder
 	}
 };
